878829b7f3435120d9a0dfd24a08896e02eb252d
Villu Ruusmann <villu.ruusmann@gmail.com>
2017-06-13 16:13:06 +0300

    Updated POM to inherit from JPMML-Evaluator library POM

--- src/main/java/org/jpmml/evaluator/spark/PMMLTransformer.java
+++ src/main/java/org/jpmml/evaluator/spark/PMMLTransformer.java
@@ -1,22 +1,22 @@
 /*
  * Copyright (c) 2016 Villu Ruusmann
  *
- * This file is part of JPMML-Spark
+ * This file is part of JPMML-Evaluator
  *
- * JPMML-Spark is free software: you can redistribute it and/or modify
+ * JPMML-Evaluator is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * JPMML-Spark is distributed in the hope that it will be useful,
+ * JPMML-Evaluator is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with JPMML-Spark.  If not, see <http://www.gnu.org/licenses/>.
+ * along with JPMML-Evaluator.  If not, see <http://www.gnu.org/licenses/>.
  */
-package org.jpmml.spark;
+package org.jpmml.evaluator.spark;
 
 import java.io.Serializable;
 import java.util.ArrayList;
